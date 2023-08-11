aa7de559d5fc506dd50ddd7abbe594e789dadb18
BhavyaKachhadiya <kachhadiyabhavya@gmail.com>
2023-08-11 15:00:05 +0530

    changes

--- Practial-2/pr9.cpp
+++ Practial-2/pr9.cpp
@@ -6,5 +6,5 @@ int main()
     cout << "The Sum of 2 Number :: " << ::a+b << endl;
     return 0;
     cout << "This code was created by Bhavya Kachhadiya." << endl;
-  cout << "Erollment number: 220130318020" << endl;
+    cout << "Erollment number: 220130318020" << endl;
 }
