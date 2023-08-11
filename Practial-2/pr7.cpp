91eb61b74ade17e0a794cd49b289a865ba9d2cd7
BhavyaKachhadiya <kachhadiyabhavya@gmail.com>
2023-08-11 15:08:17 +0530

    Changes

--- Practial-2/pr7.cpp
+++ Practial-2/pr7.cpp
@@ -3,12 +3,11 @@ using namespace std;
 int main()
 {
     enum {f,t};
-
     cout << "AND GATE" << endl;
     cout <<f<<"  "<<f<<"  "<<f*f<<endl;
     cout <<f<<"  "<<t<<"  "<<f*t<<endl;
     cout <<t<<"  "<<f<<"  "<<t*f<<endl;
     cout <<t<<"  "<<t<<"  "<<t*t<<endl;
     cout << "This code was created by Bhavya Kachhadiya." << endl;
-  cout << "Erollment number: 220130318020" << endl;
+    cout << "Erollment number: 220130318020" << endl;
 }
