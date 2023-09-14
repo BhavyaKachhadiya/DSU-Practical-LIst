bf66a98fd22fa1a9bdbc592ffe04cc40af669ab4
Bhavya <79153429+BhavyaKachhadiya@users.noreply.github.com>
2023-09-14 08:39:39 +0530

    Update p9.cpp

--- p9.cpp
+++ p9.cpp
@@ -5,24 +5,33 @@ class Max{
 	private:
 		int num_1,num_2;
 	public:
+		int largest()
+		{
+			if(num_1>num_2)
+			{
+				return num_1;
+			}
+			else
+			{
+				return num_2;
+			}
+		}
 		void input()
 		{
-			cout<<"Enter 1 Number :: ";
+			cout<<"Enter 1 Number ::";
 			cin>>num_1;
-			cout<<"Enter 2 Number :: ";
+			cout<<"Enter 2 Number ::";
 			cin>>num_2;
 		}
 		void display()
 		{
-			
-		}
-		void largest()
-		{
-			
+			int max=largest();
+			cout<<"Max :: "<<max;
 		}
 };
 int main()
 {
 	Max m;
 	m.input();
+	m.display();
 }
