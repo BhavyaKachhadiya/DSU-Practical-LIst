40902b63066c993ff6f51e11001a33c9cb808f67
Bhavya <79153429+BhavyaKachhadiya@users.noreply.github.com>
2023-09-14 08:39:04 +0530

    Add files via upload

---
+++ p11.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+class product{
+	private:
+		int product_id,unit_price;
+		string product_name;
+	public:
+		void getData()
+		{
+			cout<<"Product ID :: ";
+			cin>>product_id;
+			cout<<"Product Name :: ";
+			cin>>product_name;
+			cout<<"Product Unit Price :: ";
+			cin>>unit_price;
+			cout<<endl;
+		}
+		void putData()
+		{
+			cout<<"ID :: "<<product_id<< endl;
+			cout<<"Name :: "<<product_name<<endl;
+			cout<<"Price :: "<<unit_price<<endl<<endl;
+		}
+};
+
+int main()
+{
+	product p1,p2;
+	p1.getData();
+	p2.getData();
+	
+	p1.putData();
+	p2.putData();
+}
