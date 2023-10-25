73ded820450f2e58db8a45760cfc3aad1deeb305
BhavyaKachhadiya <kachhadiyabhavya@gmail.com>
2023-10-25 20:56:14 +0530

    [Optimisation] Convert disp() to constructor

--- Practical-4/pr5.cpp
+++ Practical-4/pr5.cpp
@@ -6,16 +6,15 @@ public:
    int n=100;
    char ch='A';
    
-   void disp(A a)
+   A()
    {
-      cout<<a.n<<endl;
-      cout<<a.ch<<endl;
+      cout<<n<<endl;
+      cout<<ch<<endl;
    }
 };
 int main() 
 {
    A obj;
-   obj.disp(obj);
    cout <<endl<<"This code was created by Bhavya Kachhadiya." << endl;
 cout << "Erollment number: 220130318020" << endl;
    return 0;
